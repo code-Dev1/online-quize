22f192f5c7d9ff9da04a53953750d6745b75e3e0
shahzada <shahzadahamidy1@gmail.com>
2024-10-01 11:32:35 -0700

    forgit password page jquery

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,15 @@
-$("#nav-bar").click(function (e) { 
-    e.preventDefault();
-    $("#nav-bar").toggleClass("active");
-    
+$(document).ready(function () {
+    // forgit password page jquery start
+    $(".emailSub").click(function (e) { 
+        e.preventDefault();
+        $("#forgitPass").addClass('d-none');
+        $("#newPass").addClass('d-none');
+        $("#OTP").removeClass("d-none");
+    });
+    $("#otpSub").click(function (e) { 
+        e.preventDefault();
+        $("#OTP").addClass("d-none");
+        $("#newPass").removeClass("d-none");
+    });
+    // forgit password page jquery end
 });
